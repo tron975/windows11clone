8b9a5d3f9093ecdc0ef4e7fc129be26efb7bb98c
tron975 <winitthepooh@gmail.com>
2021-08-28 23:31:45 -0700

    add

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 popup = document.querySelector(".pop-up")
 var logo = document.getElementsByClassName("h1");
 
-document.getElementById("h1").addEventListener('click', open)
+document.getElementById("h1").addEventListener('click', open);
+document.getElementById("search").addEventListener("click", open)
 
 function open() {
     if (popup.style.display == "none") {
         popup.style.display = 'block';
-        popup.style.animation = "zoom 0.3s"
+        popup.style.animation = "zoom 0.3s";
+        document.getElementById('search-bar').style.display = "block"
     }
 
     else {
