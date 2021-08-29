83d72fc8af599843327e8136fd1963d723d58055
tron975 <winitthepooh@gmail.com>
2021-08-28 21:31:37 -0700

    fix popup

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 popup = document.querySelector(".pop-up")
+var logo = document.getElementsByClassName("h1");
+
+document.getElementById("h1").addEventListener('click', open)
 
 function open() {
-    if (popup.style.display = "none") {
-    popup.style.display = "block"
+    if (popup.style.display == "none") {
+        popup.style.display = 'block';
+        popup.style.animation = "zoom 0.3s"
     }
+
     else {
-        popup.style.display = "none"
+        popup.style.display = "none";
+        popup.style.animation = "close 0.3s"
+        
     }
 }
